c48423f6c495f231d15e5439996691fae5541167
JPgitidaho <juanichile2015@gmail.com>
2025-11-07 12:57:28 -0300

    actualization

--- src/components/SEOTags.jsx
+++ src/components/SEOTags.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet-async"
 
 export default function SEOTags({ title, description, canonical }) {
   const schemaData = {
@@ -15,28 +14,10 @@ export default function SEOTags({ title, description, canonical }) {
   }
 
   return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <link rel="canonical" href={canonical} />
-
-
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content="https://crecermarketing.vercel.app/assets/1.png" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={canonical} />
-      <meta property="og:site_name" content="Crecer Marketing" />
-      <meta property="og:locale" content="es_CL" />
-
-      
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="https://crecermarketing.vercel.app/assets/1.png" />
-
-     
-      <script type="application/ld+json">{JSON.stringify(schemaData)}</script>
-    </Helmet>
+    <head>
+      {title && <title>{title}</title>}
+      {description && <meta name="description" content={description} />}
+      {canonical && <link rel="canonical" href={canonical} />}
+    </head>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { HelmetProvider } from 'react-helmet-async'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 )
